d586820c84b14ad24bb3972955c39f3ac72808e0
emily-li <emily.li@live.co.uk>
2025-02-27 17:36:09 -0800

    Refactor out idea generator

--- src/components/DoodlePage.tsx
+++ src/components/DoodlePage.tsx
@@ -1,20 +1,12 @@
 import { useState } from "react";
-
-const generateIdea = (): string => {
-  const ideas = [
-    "a cat with a hat",
-    "a dog with a frog",
-    "a bear with a pear",
-    "a fish with a dish",
-    "a mouse with a house",
-    "a fox with some socks",
-  ];
-  const randomIndex = Math.floor(Math.random() * ideas.length);
-  return ideas[randomIndex];
-};
+import { generateIdea } from "../utils/ideaGenerator.ts";
 
 export default function DoodlePage() {
   const [idea, setIdea] = useState<string>("");
+
+  const updateIdea = (): void => {
+    setIdea(generateIdea());
+  }
 
   /*
    * Check colours at 
@@ -26,8 +18,8 @@ export default function DoodlePage() {
       <h1 className="text-5xl text-rose-700 font-bold mb-8">doodler</h1>
 
       <button
-        className={`bg-emerald-600 text-backdrop py-2 px-4 text-2xl rounded-lg cursor-pointer active:bg-emerald-700 active:translate-1`}
-        onClick={() => setIdea(generateIdea())}
+        className="bg-emerald-600 text-backdrop py-2 px-4 text-2xl rounded-lg cursor-pointer active:bg-emerald-700 active:translate-1"
+        onClick={updateIdea}
       >
         generate doodle idea
       </button>
