a2ab84dad90b2a9abb4cfec8fb4a224de3a3c148
emily-li <emily.li@live.co.uk>
2025-02-27 20:42:07 -0800

    Add font

--- src/components/DoodlePage.tsx
+++ src/components/DoodlePage.tsx
@@ -6,25 +6,27 @@ export default function DoodlePage() {
 
   const updateIdea = (): void => {
     setIdea(generateIdea());
-  }
+  };
 
   /*
-   * Check colours at 
+   * Check colours at
    * https://webaim.org/resources/contrastchecker/
    * https://tailwindcss.com/docs/colors
    */
   return (
-    <div className="text-center p-8 font-comic min-h-screen bg-backdrop">
-      <h1 className="text-5xl text-rose-700 font-bold mb-8">doodler</h1>
+    <div className="font-display text-center p-8 font-comic min-h-screen bg-backdrop">
+      <div className="max-w-md mx-auto">
+        <h1 className="text-9xl text-rose-700 font-bold mb-8">doodler</h1>
 
-      <button
-        className="bg-emerald-600 text-backdrop py-2 px-4 text-2xl rounded-lg cursor-pointer active:bg-emerald-700 active:translate-0.5"
-        onClick={updateIdea}
-      >
-        generate doodle idea
-      </button>
-      <br />
-      {idea && <p className="mt-8 text-3xl text-cyan-900">{idea}</p>}
+        <button
+          className="container py-2 px-4 text-2xl rounded-lg cursor-pointer  active:translate-0.5 bg-emerald-600 text-backdrop active:bg-emerald-700"
+          onClick={updateIdea}
+        >
+          generate doodle idea
+        </button>
+        <br />
+        {idea && <p className="mt-8 text-5xl text-cyan-900">{idea}</p>}
+      </div>
     </div>
   );
 }
