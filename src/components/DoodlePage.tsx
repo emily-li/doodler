7499c2c92b60fecc6defd149eb2d3e3c90233d72
emily-li <emily.li@live.co.uk>
2025-02-27 17:24:46 -0800

    Change colours

--- src/components/DoodlePage.tsx
+++ src/components/DoodlePage.tsx
@@ -16,17 +16,19 @@ const generateIdea = (): string => {
 export default function DoodlePage() {
   const [idea, setIdea] = useState<string>("");
 
+  /** Validate colours at https://webaim.org/resources/contrastchecker/ */
   return (
-    <div className="text-center p-8 font-comic bg-blue-50 min-h-screen">
-      <h1 className="text-5xl text-pink-500 font-bold mb-8">doodler</h1>
+    <div className="text-center p-8 font-comic min-h-screen bg-backdrop">
+      <h1 className="text-5xl text-rose-700 font-bold mb-8">doodler</h1>
+
       <button
-        className="bg-pink-500 text-white py-2 px-4 text-2xl rounded-lg shadow-lg transform transition-transform duration-200 hover:scale-110"
+        className={`bg-emerald-600 text-backdrop py-2 px-4 text-2xl rounded-lg cursor-pointer active:bg-emerald-700 active:translate-1`}
         onClick={() => setIdea(generateIdea())}
       >
         generate doodle idea
       </button>
       <br />
-      {idea && <p className="mt-8 text-3xl text-orange-500">{idea}</p>}
+      {idea && <p className="mt-8 text-3xl text-cyan-900">{idea}</p>}
     </div>
   );
 }
