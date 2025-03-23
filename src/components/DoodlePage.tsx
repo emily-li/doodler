a0bc4fa031658df9f0fabb89de2b4b8dc22997f6
emily-li <emily.li@live.co.uk>
2025-03-23 11:26:18 -0700

    Add gradients to page

--- src/components/DoodlePage.tsx
+++ src/components/DoodlePage.tsx
@@ -24,9 +24,11 @@ export default function DoodlePage() {
    * https://tailwindcss.com/docs/colors
    */
   return (
-    <div className="flex flex-col text-center p-8 h-screen bg-backdrop">
+    <div className="flex flex-col text-center p-8 h-screen bg-gradient-to-b from-yellow-100 to-yellow-200">
       <div className="font-display max-w-md mx-auto">
-        <h1 className="text-9xl text-rose-700 font-bold mb-4">doodler</h1>
+        <h1 className="text-9xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-r from-rose-700 to-rose-900">
+          doodler
+        </h1>
 
         <DoodleIdeaButton onClick={updateIdea} />
         <DoodleIdea idea={isLoadingIdea ? "..." : idea} />
