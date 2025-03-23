7489a46ec3d3069ebd40c02964d0d51fab7af3ac
emily-li <emily.li@live.co.uk>
2025-03-23 11:48:36 -0700

    Remove backdrop variable to make it more obvious what colours are in use

--- src/components/DoodleIdeaButton.tsx
+++ src/components/DoodleIdeaButton.tsx
@@ -7,7 +7,7 @@ interface DoodleIdeaButtonProps {
 export default function DoodleIdeaButton(props: DoodleIdeaButtonProps) {
   return (
     <button
-      className="container py-2 px-4 text-2xl rounded-lg cursor-pointer active:translate-0.5 bg-emerald-600 text-backdrop active:bg-emerald-700"
+      className="container py-2 px-4 text-2xl rounded-lg cursor-pointer active:translate-0.5 bg-emerald-600 text-yellow-100 active:bg-emerald-700"
       onClick={props.onClick}
     >
       generate doodle idea
